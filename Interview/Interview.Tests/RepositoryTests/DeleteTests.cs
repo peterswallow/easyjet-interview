bcb9207921f6c0f924c3c6b2081d427651d0a400
Peter Swallow <peter.swallow@gmail.om>
2020-01-15 14:33:01 +0000

    Added failing tests for Delete() method

---
+++ Interview/Interview.Tests/RepositoryTests/DeleteTests.cs
@@ -0,0 +1,64 @@
+﻿using Interview.Entities;
+using Interview.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Interview.Tests
+{
+    public class DeleteTests
+    {
+        [Fact]
+        public void Delete_GivenItemIsNull_ThrowsExecption()
+        {
+            var repository = new Repository<IStoreable<string>, string>();
+
+            Assert.Throws<NullReferenceException>(() => { repository.Delete(null); });
+        }
+
+        [Fact]
+        public void Delete_GivenItemWithIdThatDoesNotExist_ShouldThrowException()
+        {
+            var repository = new Repository<IStoreable<string>, string>(
+                new List<IStoreable<string>> {
+                    new Storeable<string> { Id = "1" }
+                });
+
+            Assert.Throws<InvalidOperationException>(() => {
+                repository.Delete("2");
+            });
+        }
+
+        [Fact]
+        public void Delete_GivenItemWithIdThatDoesExist_ShouldRemoveAnItem()
+        {
+            var repository = new Repository<IStoreable<string>, string>(
+                new List<IStoreable<string>> {
+                    new Storeable<string> { Id = "1" },
+                    new Storeable<string> { Id = "2" }
+                });
+
+            repository.Delete("1");
+
+            Assert.Single(repository.GetAll());
+        }
+
+        [Fact]
+        public void Delete_GivenItemWithIdThatDoesExist_ShouldRemoveTheItemWithId()
+        {
+            var idToRemove = "1";
+            var repository = new Repository<IStoreable<string>, string>(
+                new List<IStoreable<string>> {
+                    new Storeable<string> { Id = idToRemove },
+                    new Storeable<string> { Id = "2" }
+                });
+
+            repository.Delete(idToRemove);
+
+            Assert.False(repository.GetAll().Any(item => item.Id == idToRemove));
+        }
+    }
+}
