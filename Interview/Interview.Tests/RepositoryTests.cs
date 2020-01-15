4487bb383e7f0cd55b3689e2783a3ace5e9dd42d
Peter Swallow <peter.swallow@gmail.om>
2020-01-15 09:01:02 +0000

    Created empty Repository and Product class with Unit Test for GetAll()

---
+++ Interview/Interview.Tests/RepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Interview.Tests
+{
+    public class RepositoryTests
+    {
+        [Fact]
+        public void GetAll_WhenCalled_ReturnsArrayWithItems()
+        {
+            var repository = new Repository<Product, int>();
+
+            var result = repository.GetAll();
+
+            Assert.Equal(2, result.Count());
+        }
+    }
+}
